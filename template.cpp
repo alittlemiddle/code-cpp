c8541c87c9e79e1f810e72c18eb9bbb81601e929
alittlemiddle <140135478+alittlemiddle@users.noreply.github.com>
2023-07-21 10:43:38 +0700

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#define el '\n'
+#define fi first
+#define sc second
+#define int ll
+#define pii pair<int, int>
+#define all(v) v.begin(), v.end()
+using namespace std;
+using ll=long long;
+using ull=unsigned long long;
+using ld=long double;
+const int mod=1e9+7;
+const int N=1e5+11;
+void sol()
+{
+    
+}
+signed main()
+{
+//    freopen("divisor.INP", "r", stdin);
+//    freopen("divisor.OUT", "w", stdout);
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int t=1;
+    //cin >> t;
+    while(t--)
+    {
+        sol();
+    }
+}
